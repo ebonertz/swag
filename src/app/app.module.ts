b7a673a28b12effb9458ed68ce6dabf6f3c1237f
Evan Bonertz <evan.bonertz@commercetools.com>
2017-10-05 16:35:33 -0400

    Add Nav Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule }  from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { PdpComponent } from './pdp/pdp.component';
+import { NavComponent } from './nav/nav.component';
+
+const routes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PdpComponent
+    PdpComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
