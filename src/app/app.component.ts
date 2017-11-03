9b55a084cf7f7670faa3f77fd1ebc9df9d490c67
Evan Bonertz <evan.bonertz@commercetools.com>
2017-11-03 14:40:53 -0400

    Install commercetools packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import { createClient } from '@commercetools/sdk-client'
+import { createAuthMiddlewareForClientCredentialsFlow } from '@commercetools/sdk-middleware-auth'
+import { createHttpMiddleware } from '@commercetools/sdk-middleware-http'
+import { createRequestBuilder } from '@commercetools/api-request-builder'
+
+var config = {
+  host: '',
+  projectKey: '',
+  credentials: {
+    clientId: '',
+    clientSecret: '',
+  },
+}
+
+var host = {
+  host: '',
+  }
 
 export class Product {
   id: number;
